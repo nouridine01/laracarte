8fb80ee2e25f2e65d39dec93af6efa48bba3214b
nouridine oumarou <nouridine27041998@gmail.com>
2019-06-30 16:44:10 +0000

    add set_active_route helper

--- resources/views/layouts/partials/_nav.blade.php
+++ resources/views/layouts/partials/_nav.blade.php
@@ -6,14 +6,14 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="nav navbar-nav mr-auto">
-      <li class="nav-item active">
+      <li class="nav-item {{set_active_route('home')}}">
         <a class="nav-link" href="{{route('home')}}">Home <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item {{set_active_route('about')}}">
         <a class="nav-link" href="{{route('about')}}">About</a>
       </li>
 
-      <li class="nav-item">
+      <li class="nav-item ">
         <a class="nav-link" href="#">Artisans</a>
       </li>
       <li class="nav-item dropdown">
